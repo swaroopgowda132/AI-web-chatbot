30fd23baf73530df10ff2e45e1568c18f9eaa489
swaroopgowda132 <swaroopg363@gmail.com>
2026-06-04 14:52:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,5 +62,11 @@ def clear():
         "status": "cleared"
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080)),
+        debug=False
+    )
