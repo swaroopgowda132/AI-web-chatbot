4b3c35b6c534a68851c889897f03bd621f4bd7a3
swaroopgowda132 <swaroopg363@gmail.com>
2026-06-04 13:51:26 +0530

    AI chatbot project

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, jsonify
+from google import genai
+from config import API_KEY
+
+app = Flask(__name__)
+
+client = genai.Client(
+    api_key=API_KEY
+)
+
+chat_history = []
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+
+    message = request.json["message"]
+
+    chat_history.append(
+        {
+            "role": "user",
+            "parts": [
+                {"text": message}
+            ]
+        }
+    )
+
+
+    response = client.models.generate_content(
+        model="gemini-3.1-flash-lite",
+        contents=chat_history
+    )
+
+
+    chat_history.append(
+        {
+            "role": "model",
+            "parts": [
+                {"text": response.text}
+            ]
+        }
+    )
+
+
+    return jsonify({
+        "reply": response.text
+    })
+
+@app.route("/clear", methods=["POST"])
+def clear():
+
+    global chat_history
+
+    chat_history = []
+
+    return jsonify({
+        "status": "cleared"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
